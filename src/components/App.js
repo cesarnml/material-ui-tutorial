e00f3a63a68bbe14e4cc178c3524b09d8ab3e442
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2019-03-17 11:35:28 -0700

    lesson01 complete

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Header, Footer } from './layout'
+import Exercises from './exercises'
+const App = props => {
+  return (
+    <>
+      <Header />
+      <Exercises />
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/exercises/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Exercises = props => {
+  return (
+    <div>
+      <h1>Hello from Exercises</h1>
+    </div>
+  )
+}
+
+export default Exercises
